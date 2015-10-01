b2e103c44ba966c2549379fd1c8dffc91708f864
Sebastien JEAN (aka Petit) <petit@peccadilles.net>
2015-10-02 00:20:48 +0200

    [add] config.php

---
+++ conf/config.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * This file is part of MinigalNano: https://github.com/sebsauvage/MinigalNano
+ *
+ * MiniGal Nano is based on an original work by Thomas Rybak (© 2010)
+ *
+ * MinigalNano is licensed under the AGPL v3 (https://gnu.org/licenses/agpl-3.0.txt).
+ */
+
+// INSERT HERE YOUR CUSTOMIZED SETTINGS
+// for the list of possible options, peruse config-default.php
+$title = "Your Own MiniGal Nano";
+// END
+?>
